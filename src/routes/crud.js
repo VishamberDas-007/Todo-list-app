7fd05a895ed56dbc83a24b5e0fc31005af11d624
Vishamber <vishdas1111@gmail.com>
2022-11-02 23:55:04 +0530

    Added listing route

--- src/routes/crud.js
+++ src/routes/crud.js
@@ -1,15 +1,32 @@
 const express = require("express"); // importing the express package
 const routes = express.Router(); // initializing routes
 const crud = require("../controllers/crud"); // importing the crud from the controller
+const { validationResult } = require("express-validator");
+const validate = require("../validations/validate");
 
 // post request for the data insertion
-routes.post("/insert", async (req, res) => {
+routes.post("/insert", validate.recordInsertValidate, async (req, res) => {
+	//validation result
+	const errors = await validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.json(errors);
+	}
+
+	// controller result
 	let result = await crud.insert(req);
 	return res.status(result.status).json(result);
 });
 
 // patch request for the data updation
-routes.patch("/update/:id", async (req, res) => {
+routes.patch("/update/:id", validate.recordUpdateValidate, async (req, res) => {
+	//validation result
+	const errors = await validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.json(errors);
+	}
+
+	// controller result
 	let result = await crud.update(req);
 	return res.status(result.status).json(result);
 });
@@ -17,6 +34,12 @@ routes.patch("/update/:id", async (req, res) => {
 // delete request for the data deletion
 routes.delete("/delete/:id", async (req, res) => {
 	let result = await crud.deleting(req);
+	return res.status(result.status).json(result);
+});
+
+// get request for the data listing
+routes.get("/listing", async (req, res) => {
+	let result = await crud.listing(req);
 	return res.status(result.status).json(result);
 });
 
