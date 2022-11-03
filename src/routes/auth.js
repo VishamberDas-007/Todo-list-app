a1c8205b606f64c4e0c9734d1840be4878fb59dd
Vishamber <vishdas1111@gmail.com>
2022-11-03 16:30:15 +0530

    Added login validation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,19 @@
 const express = require("express"); // importing the express package
 const routes = express.Router(); // initializing routes
 const { login } = require("../controllers/auth"); // importing the register from the controller
+const validate = require("../validations/validate");
+const { validationResult } = require("express-validator");
 
 // get request for the login of the user
-routes.get("/login", async (req, res) => {
+routes.get("/login", validate.loginValidate, async (req, res) => {
+	//validation result
+	const errors = await validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.json(errors);
+	}
+
+	//controller result
 	let result = await login(req);
 	return res.status(result.status).json(result);
 });
